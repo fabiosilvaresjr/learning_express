579264c61d507bbcfaac98842f0ce3fd446af267
Fabio <seu_email_fabioasilvares@gmail.com>
2025-12-30 09:09:28 -0300

    feat: implementa leitura de dados do body via POST e corrige formulário

--- index.js
+++ index.js
@@ -4,26 +4,44 @@ const port = 3000 // variável ambiente
 
 const path = require("path")
 
+//ler o body do html
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+
+app.use(express.json())
+
 const basePath = path.join(__dirname, 'templates')
 
-const checkAuth = function(req, res, next) {
-
-    req.authStatus = true
-
-    if (req.authStatus) {
-        console.log('Está logado, pode continuar')
-        next()
-    } else {
-        console.log('Não está logado, faça o login para continuar')
-        next()
-    }
-}
-
-app.use(checkAuth)
-
 app.get('/', (req, res) => {
-
     res.sendFile(`${basePath}/index.html`)
+})
+
+app.get('/users/add', (req, res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+    
+    console.log(`O nome do usuário é ${name} e ele tem ${age}`)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    //poderia ser para ler tabela de users, resgatar um usuário do banco
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
 })
 
 app.listen(port, () => {
@@ -31,3 +49,4 @@ app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 
 })
+
